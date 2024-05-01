7621ad85cb614a772b2b32748262b880dc601c6c
jerickac <joaoerickacr@gmail.com>
2024-05-01 16:15:28 -0300

    Added Prep2 Activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string strGrade = Console.ReadLine();
+
+        int grade = int.Parse(strGrade);
+        
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {  
+            Console.WriteLine("Congratulations! You were approved!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry. You didn't get a passing grade. Please try again next semester");
+        }
+
+        Console.Write($"You've got an/a {letter}");
     }
 }
